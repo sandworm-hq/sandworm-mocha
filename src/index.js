010027c209a0ceb4cf592047632a0e4b31aeb2a9
Gabi Dobocan <gabi@telepat.io>
2022-09-27 17:00:24 +0300

    feat: extract common utils to separate module

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 const Sandworm = require('sandworm');
 const path = require('path');
 const {
-  recordSandwormActivity,
-  stopRecordingSandwormActivity,
-  getRecordedActivity,
-} = require('./recorder');
-const {
-  loadDependencies,
-  loadConfig,
-  writePermissions,
-  loadPermissions,
-  SANDWORM_PERMISSION_FILE_NAME,
-} = require('./files');
-const {getPermissionsFromActivity, comparePermissions} = require('./permissions');
-const logger = require('./logger');
+  recorder: {recordSandwormActivity, stopRecordingSandwormActivity, getRecordedActivity},
+  files: {loadDependencies, loadConfig, writePermissions, loadPermissions, SANDWORM_PERMISSION_FILE_NAME},
+  permissions: {getPermissionsFromActivity, comparePermissions},
+  logger,
+} = require('sandworm-utils');
 
 const appPath = process.env.SANDWORM_APP_PATH || path.join(__dirname, '..', '..', '..');
 let config;
