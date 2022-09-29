a00a68775498d01af5610eeec6b477b5068f75cd
Gabi Dobocan <gabi@telepat.io>
2022-09-29 17:26:44 +0300

    feat: update shared `sandworm-utils` functionality

--- src/index.js
+++ src/index.js
@@ -1,32 +1,16 @@
-const Sandworm = require('sandworm');
 const path = require('path');
 const {
   recorder: {recordSandwormActivity, stopRecordingSandwormActivity, getRecordedActivity},
   files: {loadDependencies, loadConfig, writePermissions, loadPermissions, SANDWORM_PERMISSION_FILE_NAME},
-  permissions: {getPermissionsFromActivity, comparePermissions},
+  permissions: {getPermissionsFromActivity, getPackagePermissions, comparePermissions},
+  sandworm: {loadSandworm},
   logger,
 } = require('sandworm-utils');
 
 const appPath = process.env.SANDWORM_APP_PATH || path.join(__dirname, '..', '..', '..');
-let config;
+const config = loadConfig(appPath);
 
-try {
-  config = loadConfig(appPath);
-  if (config) {
-    logger.log('Config loaded');
-  }
-} catch (error) {
-  logger.log('Error loading config:', error.message);
-}
-
-// Load Sandworm
-logger.log('Setting up intercepts...');
-Sandworm.init({
-  devMode: true,
-  trustedModules: ['mocha'],
-  aliases: config && config.aliases,
-});
-logger.log('Intercepts ready');
+loadSandworm({config, trustedModules: ['mocha']});
 
 module.exports = {
   mochaHooks: {
@@ -53,58 +37,21 @@ module.exports = {
               config && Array.isArray(config.ignoredModules) ? config.ignoredModules : [];
             const permissions = getPermissionsFromActivity(activity);
 
-            const newPermissions = permissions
-              // Filter out dev dependencies
-              .filter(({module}) => {
-                const moduleNames = module.split('>');
-                const devModules = moduleNames.filter(
-                  (name) => devDependencies.includes(name) && !prodDependencies.includes(name),
-                );
-                return devModules.length === 0;
-              })
-              // Filter out explicitly ignored modules
-              .filter(({module}) => !ignoredModules.includes(module));
-
             const currentPermissions = loadPermissions(appPath);
+            const newPermissions = getPackagePermissions({
+              permissions,
+              devDependencies,
+              prodDependencies,
+              ignoredModules,
+            });
 
             if (!currentPermissions) {
               writePermissions(appPath, newPermissions, () => {
-                logger.warn("It looks like this is the first time you're running Sandworm.");
-                logger.warn(
-                  "A `package-permissions.json` file has been created in your app's root directory, with the per-module permissions detected during your test suite run.",
-                );
-                logger.warn(
-                  'Please audit this file to understand why each item is required, then commit the file to your repository.',
-                );
-                logger.warn('The next test run will validate permissions against this snapshot.');
+                logger.logTestPluginFirstRunMessage();
                 done();
               });
             } else {
-              const changes = comparePermissions(currentPermissions, newPermissions);
-              const messages = [];
-
-              changes.forEach(({module, type, permission}) => {
-                switch (type) {
-                  case 'removed-module':
-                    messages.push(`  * Existing \`${module}\` module is no longer in use`);
-                    break;
-                  case 'added-module':
-                    messages.push(`  * New module \`${module}\` has been added`);
-                    break;
-                  case 'removed-permission':
-                    messages.push(
-                      `  * Permission \`${permission}\` is no longer in use for module \`${module}\``,
-                    );
-                    break;
-                  case 'added-permission':
-                    messages.push(
-                      `  * Permission \`${permission}\` has been added to module \`${module}\``,
-                    );
-                    break;
-                  default:
-                    break;
-                }
-              });
+              const {changes, messages} = comparePermissions(currentPermissions, newPermissions);
 
               if (changes.length === 0) {
                 logger.success('✔ Permission snapshot matches current test run');
